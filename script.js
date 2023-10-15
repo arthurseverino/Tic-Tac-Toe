72ae8d70b433197a0fd1875cc6094c720f6902b0
Arthur Severino <aas1181@nyu.edu>
2023-10-15 13:42:14 -0400

    add display for turns

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 /*
 try to put everything in classes
 you want the least amount of global variables possible 
-make modal look nicer
+Have a display for turn 
 */
 const cells = document.querySelectorAll(".cell");
 const winnerHeading = document.querySelector(".winnerHeading");
 const modal = document.querySelector("dialog");
 const restartBtn = document.querySelector("#restartBtn");
+const turnDisplay = document.querySelector(".turnDisplay");
 const winningArray = [
   ["1", "2", "3"],
   ["1", "4", "7"],
@@ -17,18 +18,14 @@ const winningArray = [
   ["4", "5", "6"],
   ["7", "8", "9"],
 ];
-let round = 0;
 let movesX = [];
 let movesO = [];
+let round = 0;
 let turn = true; // X's turn
 
 function checkWin() {
   for (let array of winningArray) {
-    if (round === 9) {
-      modal.showModal();
-      winnerHeading.textContent = "It's a tie!";
-    }
-    // flip the logic, if every element in winning array is in our moves it should work
+    // if every element in winningArray is in our moves, it's a win
     if (movesX.length >= 3) {
       if (array.every((num) => movesX.includes(num))) {
         modal.showModal();
@@ -41,6 +38,14 @@ function checkWin() {
         winnerHeading.textContent = "O wins!";
       }
     }
+    if (
+      round === 9 &&
+      !array.every((num) => movesO.includes(num)) &&
+      !array.every((num) => movesX.includes(num))
+    ) {
+      modal.showModal();
+      winnerHeading.textContent = "It's a tie!";
+    }
   }
 }
 
@@ -49,21 +54,23 @@ cells.forEach((cell) => {
     if (cell.textContent === "") {
       if (turn) {
         cell.textContent = "X";
+        turnDisplay.textContent = `Current Turn: O`;
         movesX.push(cell.id);
       } else {
         cell.textContent = "O";
+        turnDisplay.textContent = `Current Turn: X`;
         movesO.push(cell.id);
       }
       turn = !turn;
-      console.log("movesX: " + movesX);
-      console.log("movesO: " + movesO);
       round++;
       checkWin();
+      console.log("movesX: " + movesX);
+      console.log("movesO: " + movesO);
     }
   });
 });
 
-//it's resolved if you define function inside eventListener
+//it's resolved if you close modal inside eventListener
 restartBtn.addEventListener("click", () => {
   movesX = [];
   movesO = [];
@@ -72,5 +79,6 @@ restartBtn.addEventListener("click", () => {
   cells.forEach((cell) => {
     cell.textContent = "";
   });
+  turnDisplay.textContent = "Click a box to play again!";
   modal.close();
 });
