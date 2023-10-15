02a0ca022b2c47eb005b2ae49146f79a84f69793
Arthur Severino <aas1181@nyu.edu>
2023-10-15 12:40:47 -0400

    finish project

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 /*
 try to put everything in classes
 you want the least amount of global variables possible 
-Have a X wins! / O wins! display on modal
+make modal look nicer
 */
-
-let movesX = [];
-let movesO = [];
-let turn = true; // X's turn
-
-// Store player moves in an array.
-// Check after every round beginning at 3rd round if that
-// array contains any 3 of the same numbers in winningArray
+const cells = document.querySelectorAll(".cell");
+const winnerHeading = document.querySelector(".winnerHeading");
+const modal = document.querySelector("dialog");
+const restartBtn = document.querySelector("#restartBtn");
 const winningArray = [
   ["1", "2", "3"],
   ["1", "4", "7"],
@@ -21,48 +17,36 @@ const winningArray = [
   ["4", "5", "6"],
   ["7", "8", "9"],
 ];
+let round = 0;
+let movesX = [];
+let movesO = [];
+let turn = true; // X's turn
 
-let winnerHeading = document.querySelector(".winnerHeading");
-
-//if you pick somewhwere that's not a perfect 3 it doesn't work
 function checkWin() {
   for (let array of winningArray) {
     if (round === 9) {
-      // modal.showModal();
-      modal.style.display = "block";
+      modal.showModal();
       winnerHeading.textContent = "It's a tie!";
     }
-    //if it's equal to 3 this works
-    // if it's >= 3, than we have nums in our array like [3,5,6,7]
-    //you still picked [3,5,7] so you should win but because of that 6 you picked earlier it doesn't let you...
-    // and you dont want to pop it or delete it because it can be a part of another move
-    //what if you sort the array, pop the non-matching elements out, check if they are equal, if not then put popped element back in array
-    //The max possible moves X can have is 5
-    // The max possible moves O can have is 4
-    if (movesX.length === 3) {
-      if (movesX.every((num) => array.includes(num))) {
-        // modal.showModal();
-        modal.style.display = "block";
+    // flip the logic, if every element in winning array is in our moves it should work
+    if (movesX.length >= 3) {
+      if (array.every((num) => movesX.includes(num))) {
+        modal.showModal();
         winnerHeading.textContent = "X wins!";
       }
     }
-    if (movesO.length === 3) {
-      if (movesO.every((num) => array.includes(num))) {
-        // modal.showModal();
-        modal.style.display = "block";
+    if (movesO.length >= 3) {
+      if (array.every((num) => movesO.includes(num))) {
+        modal.showModal();
         winnerHeading.textContent = "O wins!";
       }
     }
   }
 }
 
-const cells = document.querySelectorAll(".cell");
-let round = 0;
-
 cells.forEach((cell) => {
-  cell.addEventListener(
-    "click",
-    () => {
+  cell.addEventListener("click", () => {
+    if (cell.textContent === "") {
       if (turn) {
         cell.textContent = "X";
         movesX.push(cell.id);
@@ -75,14 +59,12 @@ cells.forEach((cell) => {
       console.log("movesO: " + movesO);
       round++;
       checkWin();
-    },
-    { once: true }
-  );
+    }
+  });
 });
 
-function handleRestart() {
-  modal.close();
-  modal.style.display = "none";
+//it's resolved if you define function inside eventListener
+restartBtn.addEventListener("click", () => {
   movesX = [];
   movesO = [];
   turn = true;
@@ -90,10 +72,5 @@ function handleRestart() {
   cells.forEach((cell) => {
     cell.textContent = "";
   });
-}
-
-const modal = document.querySelector("dialog");
-
-//try adding a new btn right next to this one
-const restartBtn = document.querySelector("#restartBtn");
-restartBtn.addEventListener("click", handleRestart());
+  modal.close();
+});
